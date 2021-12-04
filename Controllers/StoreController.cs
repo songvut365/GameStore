137f9efa8f6929e04079b925f6e1ad0833266a98
SHIBASAN <thanatuth.euro@gmail.com>
2021-12-04 08:56:29 +0700

    make controller & for loop data to index page

--- Controllers/StoreController.cs
+++ Controllers/StoreController.cs
@@ -1,16 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Text.Encodings.Web;
-
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using System.Linq;
+using GameStore.Data;
+using GameStore.Models;
+using System.Collections.Generic;
+using System.Linq;
 namespace GameStore.Controllers
 {
   public class Store : Controller
   {
 
+    private readonly  DBContext _context;
+
+            public Store(DBContext context)
+        {
+            _context = context;
+        }
+
     //แสดงสินค้าทั้งหมดและหมวดหมู่ GET: /Store/
-    public IActionResult Index()
-    {
-      return View();
-    }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Game.ToListAsync());
+        }
 
     //แสดงสินค้าตามประเภทแบบไดนามิก GET: /Store/Type?name=Action
     public IActionResult Type(string name)
