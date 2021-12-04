98a54a25d6eebe85723a6d2b550fe530776cb358
Kridsanapong Buathongjun <69898533+ussnllmn@users.noreply.github.com>
2021-12-04 16:22:52 +0700

    Controller Edit

--- Controllers/ManagementController.cs
+++ Controllers/ManagementController.cs
@@ -1,11 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GameStore.Data;
+using GameStore.Models;
 
 namespace GameStore.Controllers
 {
   public class ManagementController : Controller
   {
+    private readonly  DBContext _context;
 
+    public ManagementController(DBContext context)
+    {
+        _context = context;
+    }
     //หน้าล็อคอิน GET: /Management/
     public IActionResult Index()
     {
@@ -35,11 +47,59 @@ namespace GameStore.Controllers
 
 
     //แก้ไขสินค้าในสต๊อกตาม id GET: /Management/Edit?id=1
-    public IActionResult Edit(int id)
-    {
-      ViewData["Id"] = id;
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
 
-      return View();
+            var game = await _context.Game.FindAsync(id);
+            if (game == null)
+            {
+                return NotFound();
+            }
+            ViewData["Id"] = id;
+            return View(game);
+        }
+
+        // POST: Game/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Youtube,Main_Image,Image1,Image2,Image3,Developer,Name,Type,Detail,Price,Amount")] Game game)
+        {
+            if (id != game.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(game);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!GameExists(game.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(game);
+        }
+    private bool GameExists(int id)
+    {
+    return _context.Game.Any(e => e.Id == id);
     }
   }
 }
