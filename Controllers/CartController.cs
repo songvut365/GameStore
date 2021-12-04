e6cc1a80b060b22bd75a002d73efd91d786748e2
Songvut Nakrong <songvut.nakrong@gmail.com>
2021-12-05 00:19:48 +0700

    Update controller for error page

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -1,9 +1,18 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using GameStore.Data;
+using GameStore.Models;
 
 namespace GameStore.Controllers
 {
-  public class Cart : Controller
+  public class CartController : Controller
   {
+    private readonly DBContext _context;
+
+    public CartController(DBContext context)
+    {
+      _context = context;
+    }
 
     //แสดงสินค้าในตระกร้า GET: /Cart/
     public IActionResult Index()
@@ -12,9 +21,14 @@ namespace GameStore.Controllers
     }
 
     //แสดงการสั่งซื้อสำเร็จ GET: /Cart/Success
-    public IActionResult Success() {
+    public IActionResult Success()
+    {
       return View();
     }
 
+    //แสดงการสั่งซื้อไม่สำเร็จ GET: /Cart/Error
+    public IActionResult Failed() {
+      return View();
+    }
   }
 }
