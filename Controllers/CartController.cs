4f63df028e3a0a63973f362269bfe6d8e444735c
Songvut Nakrong <songvut.nakrong@gmail.com>
2021-12-07 02:57:35 +0700

    Update feature order

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -31,22 +31,7 @@ namespace GameStore.Controllers
       return View(carts);
     }
 
-    public IActionResult Information() 
-    {
-      return View();
-    }
-
-    //แสดงการสั่งซื้อสำเร็จ GET: /Cart/Success
-    public IActionResult Success()
-    {
-      return View();
-    }
-
-    //แสดงการสั่งซื้อไม่สำเร็จ GET: /Cart/Error
-    public IActionResult Failed() {
-      return View();
-    }
-
+    //เพิ่มสินค้าลงในตะกร้า
     public async Task<IActionResult> AddToCart(int id) {
       Cart cart = new Cart();
       cart.gameId = id;
@@ -71,7 +56,8 @@ namespace GameStore.Controllers
 
       return RedirectToAction("Index", "Cart");
     }
-
+    
+    //ลบสินค้าออกจากตะกร้า
     public async Task<IActionResult> RemoveFromCart(int id) {
       Cart cart = new Cart();
       cart.gameId = id;
@@ -101,16 +87,62 @@ namespace GameStore.Controllers
       return RedirectToAction("Index", "Cart");
     }
 
-    //สร้างออเดอร์สั่งซื้อสินค้า POST: /Cart/Create
+    //หน้ากรอกข้อมูลยืนยันการสั่งซื้อสินค้า GET: /Cart/Information
+    public IActionResult Information() 
+    {
+      return View();
+    }
+
+    //สร้างออเดอร์สั่งซื้อสินค้า
     [HttpPost]
-    public async Task<IActionResult> Create(JsonResult order) 
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create(Order order) 
     {
       if (ModelState.IsValid) {
-        _context.Add(order);
-        await _context.SaveChangesAsync();
+        List<Cart> carts = await _context.Cart.ToListAsync();
+        
+        //1. foreach all game in cart
+        foreach(Cart cart in carts) {
+          Order temp = new Order();
+          temp.Name = order.Name;
+          temp.LastName = order.LastName;
+          temp.Phone = order.Phone;
+          temp.Email = order.Email;
+          temp.Game_Id = cart.gameId;
+          temp.Game_Amount = cart.count;
+          temp.Price_Total = cart.totalPrice;
+          
+          //2. check amount game can't be less than 1
+          Game game = await _context.Game.FirstOrDefaultAsync(g => g.Id == cart.gameId);
+          if(game.Amount > temp.Game_Amount) {
+            game.Amount -= temp.Game_Amount;
+            await _context.AddAsync(temp);
+            await _context.SaveChangesAsync();
+          }
+          else {
+            return RedirectToAction(nameof(Failed));
+          }
+        }
+
+        //3. clear game in cart
+        _context.RemoveRange(_context.Cart);
+        await _context.SaveChangesAsync();        
+        
         return RedirectToAction(nameof(Success));
       }  
       return RedirectToAction(nameof(Failed));
     }
+
+    //แสดงการสั่งซื้อสำเร็จ GET: /Cart/Success
+    public IActionResult Success()
+    {
+      return View();
+    }
+
+    //แสดงการสั่งซื้อไม่สำเร็จ GET: /Cart/Error
+    public IActionResult Failed() {
+      return View();
+    }
+
   }
 }
