f80d006f8f8b2901b9879dd7bc6b9546d74d3504
Songvut Nakrong <songvut.nakrong@gmail.com>
2021-12-07 00:56:46 +0700

    Add cart model

--- Migrations/20211205024109_CreateCartModel.Designer.cs
+++ Migrations/20211205024109_CreateCartModel.Designer.cs
@@ -2,14 +2,16 @@
 using GameStore.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GameStore.Migrations
 {
     [DbContext(typeof(DBContext))]
-    partial class DBContextModelSnapshot : ModelSnapshot
+    [Migration("20211205024109_CreateCartModel")]
+    partial class CreateCartModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
